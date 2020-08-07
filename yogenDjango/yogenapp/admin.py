b2e2eccee9ac11ea92a44e835f403dc44b46e9df
yogenns <yogenns@gmail.com>
2020-08-07 12:41:15 +0530

    Create Models

--- yogenDjango/yogenapp/admin.py
+++ yogenDjango/yogenapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from yogenapp.models import Topic,AccessRecord,WebPage
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(WebPage)
+admin.site.register(AccessRecord)
