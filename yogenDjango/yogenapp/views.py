3a52dc903cfda9f18275157aa128e29bff851f96
yogenns <yogenns@gmail.com>
2020-08-07 11:27:24 +0530

    Template Creation

--- yogenDjango/yogenapp/views.py
+++ yogenDjango/yogenapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World");
+    my_dict = {'test_var':'This is a test'}
+    return render(request,'yogenapp/index.html',context=my_dict)
